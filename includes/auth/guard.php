d5ad5fd94073bb97b831ecde5f16f2588a712448
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-16 16:53:15 +0100

    fix: enhance role verification logic to handle inactive guide users

--- includes/auth/guard.php
+++ includes/auth/guard.php
@@ -12,8 +12,15 @@ function require_login()
 
 function require_role($role_needed)
 {
-    if (!isset($_SESSION["role"]) || $_SESSION["role"] != $role_needed) {
-        header("location: ../../index.php?message=no_access");
-        exit();
+    if (!isset($_SESSION["role"]) || $_SESSION["role"] != $role_needed || $_SESSION["isActive"] == 0 && $_SESSION["role"] == "guide") {
+        if($_SESSION["isActive"] == 0 && $_SESSION["role"] == "guide"){
+            header("location: ../../pages/guide/pending.php");
+            exit();
+        }
+        else{
+            header("location: ../../index.php?message=no_access");
+            exit();
+        }
+
     }
 }
