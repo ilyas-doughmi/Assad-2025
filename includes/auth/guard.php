35e506b95e6600f6c66ce565415f89a4349b4e4c
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-16 16:15:53 +0100

    feat: add authentication guard functions for user login and role verification

---
+++ includes/auth/guard.php
@@ -0,0 +1,19 @@
+<?php
+
+session_start();
+
+function require_login()
+{
+    if (!isset($_SESSION["user_id"])) {
+        header("location: ../../login.php");
+        exit();
+    }
+}
+
+function require_role($role_needed)
+{
+    if (!isset($_SESSION["role"]) || $_SESSION["role"] == $role_needed) {
+        header("location: ../../index.php?message=no access");
+        exit();
+    }
+}
