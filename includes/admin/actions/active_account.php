52908d80704d8b481ded3a3a129bba285b90be85
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-17 14:42:37 +0100

    feat: add functionality to reactivate user accounts in admin actions

---
+++ includes/admin/actions/active_account.php
@@ -0,0 +1,19 @@
+<?php
+require_once("../../db.php");
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $user_id = $_POST["user_id"];
+    
+    $query = "UPDATE users SET isBanned = 0 WHERE id = ?";
+
+    $stmt = mysqli_prepare($conn,$query);
+    
+    if ($stmt === false) {
+        die("Error prepare statement" . mysqli_error($conn));
+    }
+
+    mysqli_stmt_bind_param($stmt,"i",$user_id);
+    mysqli_stmt_execute($stmt);
+
+    echo "Active Successfully";
+}
