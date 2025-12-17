6fbd0abc107a1a1093833b33ef118ba5b94f9a63
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-17 15:01:17 +0100

    feat: enhance habitat and animal data with detailed descriptions and additional species

--- sql/install.sql
+++ sql/install.sql
@@ -87,3 +87,60 @@ CREATE TABLE Comments (
 -- adding admin (password is admin123)
 
 INSERT INTO users(full_name,email,password,role,isActive) VALUES("Ilyas Doughmi","admin@gmail.com","$2y$10$bSuhIS36G5C7c2NuqTXUmu3aBMLh5LYQXjmmAdQQas3aL8puDy7XW","admin",1);
+
+-- inserting habitat
+INSERT INTO Habitat (nom, description, image) VALUES
+(
+    'Savane',
+    'Vaste plaine africaine caractérisée par une végétation herbacée et quelques arbres. Habitat naturel des lions, girafes et éléphants.',
+    'savane.jpg'
+),
+(
+    'Forêt tropicale',
+    'Milieu chaud et humide avec une biodiversité exceptionnelle. Abrite de nombreuses espèces d’oiseaux, de reptiles et de mammifères.',
+    'foret_tropicale.jpg'
+),
+(
+    'Désert',
+    'Zone aride avec de faibles précipitations et des températures extrêmes. Adapté aux espèces résistantes comme les dromadaires et les fennecs.',
+    'desert.jpg'
+),
+(
+    'Zone polaire',
+    'Région glaciale aux températures très basses. Habitat des ours polaires, manchots et phoques.',
+    'zone_polaire.jpg'
+),
+(
+    'Jungle',
+    'Forêt dense et humide avec une végétation luxuriante. Accueille des tigres, singes et serpents.',
+    'jungle.jpg'
+);
+
+
+-- inserting animals
+
+INSERT INTO Animal (nom, espece, pays_origin, habitat_id) VALUES
+-- Savane
+('Lion', 'Panthera leo', 'Kenya', 1),
+('Éléphant d’Afrique', 'Loxodonta africana', 'Tanzanie', 1),
+('Girafe', 'Giraffa camelopardalis', 'Afrique du Sud', 1),
+
+-- Forêt tropicale
+('Perroquet Ara', 'Ara macao', 'Brésil', 2),
+('Jaguar', 'Panthera onca', 'Amazonie', 2),
+('Grenouille dendrobate', 'Dendrobates tinctorius', 'Colombie', 2),
+
+-- Désert
+('Dromadaire', 'Camelus dromedarius', 'Maroc', 3),
+('Fennec', 'Vulpes zerda', 'Sahara', 3),
+('Scorpion jaune', 'Leiurus quinquestriatus', 'Égypte', 3),
+
+-- Zone polaire
+('Ours polaire', 'Ursus maritimus', 'Arctique', 4),
+('Manchot empereur', 'Aptenodytes forsteri', 'Antarctique', 4),
+('Phoque', 'Phoca vitulina', 'Groenland', 4),
+
+-- Jungle
+('Tigre', 'Panthera tigris', 'Inde', 5),
+('Singe capucin', 'Cebus capucinus', 'Costa Rica', 5),
+('Serpent Boa', 'Boa constrictor', 'Brésil', 5);
