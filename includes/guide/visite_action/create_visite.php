92f5ca17d33083b3625db214ae9816eea62fa91e
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-18 15:08:28 +0100

    feat: adding new file to create a visite after getting a post request

---
+++ includes/guide/visite_action/create_visite.php
@@ -0,0 +1,35 @@
+<?php
+require_once("../../db.php");
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    
+    
+    $image = $_POST['image'];
+    $titre = $_POST['title'];
+    $description = $_POST['description'];
+    $date_heure =$_POST['date'];
+    $duree = $_POST['duree'];
+    $prix = $_POST['prix'];
+    $langue = $_POST['langue'];
+    $capacite = $_POST['capacity'];
+    $guide_id = $_POST["guide_id"];
+    $status = "open";
+
+    $query = "INSERT INTO tours(titre,description,date_heure_debut,duree_minutes,prix,langue,capacity_max
+    ,guide_id,status) VALUES(?,?,?,?,?,?,?,?,?)";
+    
+    $stmt = mysqli_prepare($conn,$query);
+       if($stmt === false){
+        die("Error prepare statement".mysqli_error($conn));
+    }
+
+    mysqli_stmt_bind_param($stmt,"sssiisiis",$titre,$description,$date_heure,$duree,$prix,$langue,$capacite,$guide_id,$status);
+
+    try{
+        mysqli_stmt_execute($stmt);
+        echo "visite added successfuly";
+    }catch(mysqli_sql_exception){
+       echo "problem happend";
+    }   
+ 
+}
