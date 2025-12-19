560d16dae277f30bf3c2c2935059c3146d6ba725
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-19 09:52:28 +0100

    feat: adding a function to get tour info using it id and adding other function to get guidename using user id

--- includes/guide/visite_data.php
+++ includes/guide/visite_data.php
@@ -1,5 +1,6 @@
 <?php
-require_once("../db.php");
+require_once __DIR__ . "/../db.php";
+
 
 if(isset($_POST["gettours"])){
     echo getTours();
@@ -11,4 +12,37 @@ function getTours(){
     $run = mysqli_query($conn,$query);
     $result = mysqli_fetch_all($run,MYSQLI_ASSOC);
     return json_encode($result);
+}
+
+function getTourInfo($tour_id){
+    global $conn;
+    $query = "SELECT * FROM tours WHERE id = ?";
+    $stmt = mysqli_prepare($conn,$query);
+
+    if($stmt == false){
+        die("problem");
+    }
+
+    mysqli_stmt_bind_param($stmt,"i",$tour_id);
+
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    return mysqli_fetch_assoc($result);
+}
+
+
+function getGuideName($user_id){
+    global $conn;
+    $query = "SELECT * FROM users WHERE id = ?";
+    $stmt = mysqli_prepare($conn,$query);
+
+    if($stmt == false){
+        die("problem");
+    }
+
+    mysqli_stmt_bind_param($stmt,"i",$user_id);
+
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    return mysqli_fetch_assoc($result);
 }
